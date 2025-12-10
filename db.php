ff818451d34e0ea5626a244209f8e44b5c01b39c
UP2213066 <147258036+UP2213066@users.noreply.github.com>
2025-12-10 15:27:22 +0000

    update

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$hostname = 'database';
-$username = 'login';
-$password = 'password';
-$database = "staff";
-$table = 'staff';
-$startPassword = '$2y$12$jxbFxiKXiW9JV8ctx3CdqOIs6MBtVEdge/cnSQAZn/jPHSV9btipy';
