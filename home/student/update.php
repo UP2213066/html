0af202b002d4e226355d45831d32e8cb6334c81d
UP2213066 <147258036+UP2213066@users.noreply.github.com>
2025-12-10 20:32:48 +0000

    update

--- home/student/update.php
+++ home/student/update.php
@@ -29,5 +29,5 @@ session_unset();
 $_SESSION['name'] = $name;
 $_SESSION['role'] = $role;
 $_SESSION['email'] = $email;
-header("Location: /home/staff/");
+header("Location: /home/student/");
 die();
