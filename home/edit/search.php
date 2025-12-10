87b626c8f0348ef92d9eae57b97e226b5abeb503
UP2213066 <147258036+UP2213066@users.noreply.github.com>
2025-12-10 16:28:56 +0000

    update

--- home/edit/search.php
+++ home/edit/search.php
@@ -69,7 +69,7 @@ if ($result->num_rows > 0) {
                 <label for="role">Quota:</label>
                 <?php echo '<input type="text" id="quota" name="quota" value="' . $quota . '">' ?>
                 <input type="submit" value="Submit">
-                <a href="delete.php"><button>Delete</button></a>;
+                <a href="delete.php"><button>Delete</button></a>
             </form>
         </main>
     </body>
