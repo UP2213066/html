cd2cd5624dd73e29f851bbe56f4a50ce209cf4a1
UP2213066 <147258036+UP2213066@users.noreply.github.com>
2025-12-10 15:45:53 +0000

    update

--- home/edit/update.php
+++ home/edit/update.php
@@ -17,7 +17,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 session_start();
-var_dump($_SESSION);
 include '/sec/db.php';
 $name = trim($_POST['name']);
 $email = trim($_POST['email']);
