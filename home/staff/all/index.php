74c4eae14577a892348b6a1442526a6bef73bc01
UP2213066 <147258036+UP2213066@users.noreply.github.com>
2025-12-11 00:24:51 +0000

    update

---
+++ home/staff/all/index.php
@@ -0,0 +1,58 @@
+<!-- 
+    Project: Final Year Project Admin Web Application
+    Author: Ayden Lunnon
+    Student Number: UP2213066
+    Course: BSc (hons) Cybersecurity and Forensic Computing, University of Portsmouth
+    Year: 2025/26
+
+    Description: This file is responsible for querying the database and pulling
+    every staff member to view and edit.
+
+    © 2025 Ayden Lunnon. All rights reserved.
+    This code is submitted as part of a university project and may not be 
+    reused or redistributed without permission.
+-->
+<?php
+include '/var/www/html/validate.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FYP Student Editor</title>
+        <link rel="stylesheet" href="/style.css?v=1">
+    </head>
+    <body>
+    <nav class="navigationBar">
+        <a class="home" href="/home/">University of Portsmouth</a>
+        <a href="/logout.php">Logout</a>
+        <a href="./">Back</a>
+    </nav>
+    <main>
+        <h1>All Students</h1>
+        <?php
+        $connection = new mysqli($hostname, $username, $password, $database);
+        $preparedSQL = $connection->prepare("SELECT name, email, role, quota, allocatedStudents, studentsToAvoid FROM staff");
+        $preparedSQL->execute();
+        $result = $preparedSQL->get_result();
+        if ($result->num_rows > 0) {
+            unset($_SESSION['search-error']);
+            while ($row = $result->fetch_assoc()) {
+                $name = $row['name'];
+                $email = $row['email'];
+                $_SESSION['emailToUpdate'] = $email;
+                $role = $row['role'];
+                $quota = $row['quota'];
+                $allocatedStudents = $row['allocatedStudents'];
+                $studentsToAvoid = $row['studentsToAvoid'];
+                echo "<a href='/home/staff/search.php?email=$email'>$name - $email | Role: $role | Quota: $quota | Allocated Students: $allocatedStudents | Students to Avoid: $studentsToAvoid</a><br>";
+            }
+        } else {
+            $_SESSION['search-error'] = "Student Not Found";
+            header("Location: /home/student/");
+            die();
+        }
+        ?>
+    </main>
+    </body>
+    <?php include '/var/www/html/footer.php';?>
+</html>
