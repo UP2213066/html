beeb107ace3435fb19bed850d03d1016681905e4
UP2213066 <147258036+UP2213066@users.noreply.github.com>
2025-12-11 00:22:15 +0000

    update

--- home/student/all/index.php
+++ home/student/all/index.php
@@ -29,61 +29,44 @@ include '/var/www/html/validate.php';
     </nav>
     <main>
         <h1>All Students</h1>
-<?php
-$connection = new mysqli($hostname, $username, $password, $database);
-$preparedSQL = $connection->prepare("SELECT id, firstName, lastName, courseCode, moduleCode, supervisor, moderator FROM students");
-$preparedSQL->execute();
-$result = $preparedSQL->get_result();
-if ($result->num_rows > 0) {
-    unset($_SESSION['search-error']);
-    while ($row = $result->fetch_assoc()) {
-        $id = $row['id'];
-        $firstName = $row['firstName'];
-        $lastName = $row['lastName'];
-        $name = $firstName . ' ' . $lastName;
-        $_SESSION['nameToUpdate'] = $name;
-        $course = $row['courseCode'];
-        $module = $row['moduleCode'];
-        echo "<a href='/home/student/search.php?id=$id'>UP$id - $name | Course: $course - FYP Module: $module";
-        if (isset($row['supervisor'])) {
-            $supervisor = $row['supervisor'];
-            echo " | Supervisor: $supervisor";
+        <?php
+        $connection = new mysqli($hostname, $username, $password, $database);
+        $preparedSQL = $connection->prepare("SELECT id, firstName, lastName, courseCode, moduleCode, supervisor, moderator FROM students");
+        $preparedSQL->execute();
+        $result = $preparedSQL->get_result();
+        if ($result->num_rows > 0) {
+            unset($_SESSION['search-error']);
+            while ($row = $result->fetch_assoc()) {
+                $id = $row['id'];
+                $firstName = $row['firstName'];
+                $lastName = $row['lastName'];
+                $name = $firstName . ' ' . $lastName;
+                $_SESSION['nameToUpdate'] = $name;
+                $course = $row['courseCode'];
+                $module = $row['moduleCode'];
+                echo "<a href='/home/student/search.php?id=$id'>UP$id - $name | Course: $course - FYP Module: $module";
+                if (isset($row['supervisor'])) {
+                    $supervisor = $row['supervisor'];
+                    echo " | Supervisor: $supervisor";
+                } else {
+                    $supervisor = "";
+                    echo " | No Supervisor";
+                }
+                if (isset($row['moderator'])) {
+                    $moderator = $row['moderator'];
+                    echo " - Moderator: $moderator</a>";
+                } else {
+                    $moderator = "";
+                    echo " - No Moderator</a>";
+                }
+            }
         } else {
-            $supervisor = "";
-            echo " | No Supervisor";
+            $_SESSION['search-error'] = "Student Not Found";
+            header("Location: /home/student/");
+            die();
         }
-        if (isset($row['moderator'])) {
-            $moderator = $row['moderator'];
-            echo " - Moderator: $moderator</a>";
-        } else {
-            $moderator = "";
-            echo " - No Moderator</a>";
-        }
-    }
-} else {
-    $_SESSION['search-error'] = "Student Not Found";
-    header("Location: /home/student/");
-    die();
-}
-?>
-        <?php echo "<h1>" . $name . "</h1>" ?>
-            <form action="update.php" method="post">
-                <label for="firstName">First Name:</label>
-                <?php echo '<input type="text" id="firstName" name="firstName" value="' . $firstName . '">' ?>
-                <label for="lastName">Last Name:</label>
-                <?php echo '<input type="text" id="lastName" name="lastName" value="' . $lastName . '">' ?>
-                <label for="course">Course Code:</label>
-                <?php echo '<input type="text" id="course" name="course" value="' . $course . '">' ?>
-                <label for="module">Module Code:</label>
-                <?php echo '<input type="text" id="module" name="module" value="' . $module . '">' ?>
-                <label for="supervisor">Supervisor:</label>
-                <?php echo '<input type="text" id="supervisor" name="supervisor" value="' . $supervisor . '">' ?>
-                <label for="moderator">Moderator:</label>
-                <?php echo '<input type="text" id="moderator" name="moderator" value="' . $moderator . '">' ?>
-                <input type="submit" value="Submit">
-                <button type="submit" formaction="delete/">Delete</button>
-            </form>
-        </main>
+        ?>
+    </main>
     </body>
     <?php include '/var/www/html/footer.php';?>
 </html>
