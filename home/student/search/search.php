0ebe0a220ef717be517d2c507d63bf8eee2896f5
UP2213066 <147258036+UP2213066@users.noreply.github.com>
2026-01-15 17:13:34 +0000

    update

--- home/student/search/search.php
+++ home/student/search/search.php
@@ -71,7 +71,7 @@ if ($result->num_rows > 0) {
         $projectNames = explode(",", $row['projectNames']);
     }
     foreach($projectCodes as $code) {
-        if ($code === $module) {
+        if (trim($code) == trim($module)) {
             $index = array_search($code, $projectCodes);
             echo $index;
             $currentProjectCode = $projectCodes[$index];
