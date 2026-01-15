c2f26f08615eb0f01bfc5e17eac0745901792756
UP2213066 <147258036+UP2213066@users.noreply.github.com>
2026-01-15 18:52:19 +0000

    update

---
+++ home/account/password/update.php
@@ -0,0 +1,34 @@
+<!-- 
+    Project: Final Year Project Admin Web Application
+    Author: Ayden Lunnon
+    Student Number: UP2213066
+    Course: BSc (hons) Cybersecurity and Forensic Computing, University of Portsmouth
+    Year: 2025/26
+
+    Description: This file is responsible for updating the database using the new
+    edited data sent.
+
+    © 2025 Ayden Lunnon. All rights reserved.
+    This code is submitted as part of a university project and may not be 
+    reused or redistributed without permission.
+-->
+<?php
+include '/var/www/html/validate.php';
+$connection = new mysqli($hostname, $username, $password, $database);
+$preparedSQL = $connection->prepare("SELECT password FROM staff WHERE email = ?");
+$preparedSQL->bind_param("s", $_SESSION['email']);
+$preparedSQL->execute();
+$connection->close();
+$password = $_row['password'];
+if (password_verify($_POST['current'], $password)) {
+    if ($_POST['new1'] === $_POST['new2']) {
+        $password = password_hash($_POST['new1'], PASSWORD_DEFAULT);
+        $connection = new mysqli($hostname, $username, $password, $database);
+        $preparedSQL = $connection->prepare("UPDATE staff SET password = ? WHERE email = ?");
+        $preparedSQL->bind_param("ss", $password, $_SESSION['email']);
+        $preparedSQL->execute();
+        $connection->close();
+    }
+}
+header("Location: /logout.php");
+die();
