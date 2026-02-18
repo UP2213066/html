efa1ae6370b503a4d4651456eec4dbab5610785c
UP2213066 <147258036+UP2213066@users.noreply.github.com>
2026-02-18 17:12:21 +0000

    update

--- home/student/all/index.php
+++ home/student/all/index.php
@@ -38,6 +38,9 @@ include '/var/www/html/validate.php';
     <main>
         <h1>All Students</h1>
         <?php
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
         $connection = new mysqli($hostname, $read_student_username, $read_student_password, $database);
         if ($connection->connect_error) {
             echo "<p>Database Connection Failed</p>";
