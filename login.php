ea74b02c2967c0c81da2611370cab32b784b3be8
UP2213066 <147258036+UP2213066@users.noreply.github.com>
2026-03-19 22:11:40 +0000

    update

--- login.php
+++ login.php
@@ -57,7 +57,7 @@ if ($result->num_rows === 1) {
                 $connection->close();
                 $connection = new mysqli($hostname, $insert_attemp_username, $insert_attempt_password, $database);
                 $now = date("Y-m-d H:i:s", time());
-                $preparedSQL = $connection->prepare("INSERT INTO attempts VALUES email=?, IP=?, timestamp=?");
+                $preparedSQL = $connection->prepare("INSERT INTO failedLogins VALUES email=?, IP=?, timestamp=?");
                 $preparedSQL->bind_param("ss", $_POST['username'], $_SERVER['REMOTE_ADDR'], $now);
                 $preparedSQL->execute();
                 $connection->close();
@@ -66,6 +66,11 @@ if ($result->num_rows === 1) {
                 $connection = new mysqli($hostname, $update_staff_username, $update_staff_password, $database);
                 $preparedSQL = $connection->prepare("UPDATE staff SET attempts=? WHERE email = ?");
                 $preparedSQL->bind_param("ss", $newAttempts, $_POST['username']);
+                $preparedSQL->execute();
+                $connection->close();
+                $now = date("Y-m-d H:i:s", time());
+                $preparedSQL = $connection->prepare("INSERT INTO failedLogins VALUES email=?, IP=?, timestamp=?");
+                $preparedSQL->bind_param("ss", $_POST['username'], $_SERVER['REMOTE_ADDR'], $now);
                 $preparedSQL->execute();
                 $connection->close();
             }
